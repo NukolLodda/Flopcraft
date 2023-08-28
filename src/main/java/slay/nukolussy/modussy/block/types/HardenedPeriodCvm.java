89e3bd15ab8d6b8bd728607bf5870ee5f757a7c5
Nukol <nukollodda@gmail.com>
2023-08-27 20:35:20 -0500

    Added custom Modussy splash screens and end screens

--- src/main/java/slay/nukolussy/modussy/block/types/HardenedPeriodCvm.java
+++ src/main/java/slay/nukolussy/modussy/block/types/HardenedPeriodCvm.java
@@ -6,7 +6,6 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class HardenedPeriodCvm extends Block {
-    private static final Direction[] ALL_DIRECTIONS = Direction.values();
 
     public HardenedPeriodCvm() {
         super(BlockBehaviour.Properties.copy(Blocks.OBSIDIAN).lightLevel(l -> 4));
